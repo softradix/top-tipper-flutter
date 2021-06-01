399ff9c77d6cb07ecf90075311b2b498402ef590
artidroid1190 <artiverma693@gmai.com>
2021-06-01 15:55:14 +0530

    local text setup updated

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData().copyWith(primaryColor: orange),
       home: Splash(),
-      //text
     );
   }
 }
